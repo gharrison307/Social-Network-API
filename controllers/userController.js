19ce9681bb4f50f397b80e284e5644f5a3c1977f
George Harrison <g.a.harrison91@gmail.com>
2023-04-16 15:42:52 -0600

    started users controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,16 @@
 const { User, Thought } = require("../models/index");
+
+module.exports = {
+  // get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((error) => res.status(500).json(error));
+  },
+  // get one user
+  // add user
+  // update user
+  // delete user
+  // add friend to user
+  // delete friend
+};
