c2fa249be8e1a9a2ec64f0705098549293a74306
George Harrison <g.a.harrison91@gmail.com>
2023-04-16 15:12:21 -0600

    added more thought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -17,8 +17,65 @@ module.exports = {
       .populate("reactions")
       .then((oneThought) =>
         !oneThought
-          ? res.status(404).json({ message: "No thought with this ID" })
+          ? res.status(404).json({ message: "No thought with this ID." })
           : res.status(200).json(oneThought)
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => {
+        return User.findOneAndUpdate(
+          { username: req.body.username },
+          { $addToSet: { thoughts: thought._id } },
+          { new: true }
+        );
+      })
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: "No User with that ID. Thought was still created.",
+            })
+          : res.status(200).json({ message: "Thought has been created." })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { thoughtText: req.body.thoughtText },
+      {
+        new: true,
+      }
+    )
+      .then((updatedThought) => {
+        !updatedThought
+          ? res.status(404).json({ message: "No thought with that ID." })
+          : res.status(200).json(updatedThought);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((response) => {
+        !response
+          ? res.status(404).json({ message: "No thought with that ID." })
+          : User.findOneAndUpdate(
+              { username: req.body.username },
+              { $pull: { thoughts: { _id: req.params.thoughtId } } }
+            );
+      })
+      .then(() =>
+        res.status(200).json({ message: "Thought deleted from User" })
       )
       .catch((err) => {
         console.log(err);
